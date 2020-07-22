f6c15e9f77903d65358e37727e16109060fb81f1
Eduardo Cintra dos Santos <38438615+eduardocintrasantos@users.noreply.github.com>
2020-07-22 11:51:22 -0300

    componentMine
    Criação da mina e configurações de mina aberta e mina que explodiu.

--- campoMinado/App.js
+++ campoMinado/App.js
@@ -20,6 +20,9 @@ export default class App extends Component{
         <Field opened nearMines={4}/>
         <Field opened nearMines={6}/>
         <Field opened nearMines={8}/>
+        <Field mined />
+        <Field opened mined />
+        <Field mined opened exploded />
       </View>
     )
   }
