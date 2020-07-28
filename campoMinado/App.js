9ccea1149fe2f38a58ea028b180b4712648e3e71
Eduardo Cintra dos Santos <38438615+eduardocintrasantos@users.noreply.github.com>
2020-07-28 15:51:52 -0300

    logic and onPress
    Implementação da logica do jogo e tambem a função onPress pra precionar os campos.

--- campoMinado/App.js
+++ campoMinado/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Alert } from 'react-native';
 import params from './src/params';
 import MineField from './src/components/MineField';
-import { createMinedBoard } from './src/functions';
+import { createMinedBoard, cloneBoard, openField, hadExplosion, wonGame, showMines } from './src/functions';
 
 export default class App extends Component{
 
@@ -22,7 +22,27 @@ export default class App extends Component{
     const rows = params.getRowsAmount()
     return {
       board: createMinedBoard(rows, cols, this.minesAmount()),
+      won: false,
+      lost: false
     }
+  }
+
+  onOpenField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    openField(board, row, column)
+    const lost = hadExplosion(board)
+    const won = wonGame(board)
+
+    if (lost) {
+      showMines(board)
+      Alert.alert('Perdeu!')
+    }
+
+    if (won) {
+      Alert.alert('Ganhou!')
+    }
+
+    this.setState({ board, lost, won})
   }
 
   render() {
@@ -35,10 +55,9 @@ export default class App extends Component{
           Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
         <View style={styles.board}>
-          <MineField board={this.state.board} />
-        </View>
-
-        
+          <MineField board={this.state.board} 
+            onOpenField={this.onOpenField}/>
+        </View>      
       </View>
     )
   }
